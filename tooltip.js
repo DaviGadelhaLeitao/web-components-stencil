61526811ebf63165ed5b0fb74c1fd878370906c7
davi.leitao <dgleitao.contratos@gmail.com>
2019-03-16 11:47:08 -0300

    Using attributes on Custom Elements

--- tooltip.js
+++ tooltip.js
@@ -2,10 +2,21 @@ class Tooltip extends HTMLElement {
     constructor() {
         super();
         this._tooltipContainer;
-    }
 
+        // initializing tooptipText with some value
+        this._tooltipText = "I'm the default when you hover over me!";
+
+        // if you leave this code below what will happen it that it will be executed
+        // when wasn't inserted into the page. Therefore it won't work. The correct thing
+        // to do is to move it down below inside the connectedCallback method.
+        // this._tooltipText = this.getAttribute('text');
+    }
+    
     connectedCallback() {
         const tooltipIcon = document.createElement('span');
+        if(this.hasAttribute('text')) {
+            this._tooltipText = this.getAttribute('text');
+        }
         tooltipIcon.textContent = ' (?)';
         tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
         tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
@@ -14,7 +25,7 @@ class Tooltip extends HTMLElement {
 
     _showTooltip() {
         this._tooltipContainer = document.createElement('div');
-        this._tooltipContainer.textContent = 'This is a tooltip text';
+        this._tooltipContainer.textContent = this._tooltipText;
         this.appendChild(this._tooltipContainer);
     }
 
