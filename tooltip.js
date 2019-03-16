a9fe6c274361ea035ddee8008c2ea17aff5d5607
davi.leitao <dgleitao.contratos@gmail.com>
2019-03-16 16:41:22 -0300

    Styling our Component without a Shadow DOM
    In this commit we make use of some global css
    just to illustrate how Elements into the Light
    DOM share css rules.
    
    Then, into the next commit we'll attach the created
    element to the Shadow DOM instead of the light DOM.
    
    Therefore, our component will have it's own DOM tree
    which isn't affected by the normal DOM tree.

--- tooltip.js
+++ tooltip.js
@@ -10,6 +10,12 @@ class Tooltip extends HTMLElement {
         // when wasn't inserted into the page. Therefore it won't work. The correct thing
         // to do is to move it down below inside the connectedCallback method.
         // this._tooltipText = this.getAttribute('text');
+
+        // define if you can access this DOM tree from outside this component or not.
+        // closing if typically something you won't do because even so there are ways to access it
+        // and it simply does not worth the effort. Now this element has it's own shadow DOM tree attached
+        // to it.
+        //this.attachShadow({mode: 'open'});
     }
     
     connectedCallback() {
@@ -21,15 +27,23 @@ class Tooltip extends HTMLElement {
         tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
         tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
         this.appendChild(tooltipIcon);
+        //this.shadowRoot.appendChild(tooltipIcon);
+        this.style.position = 'relative';
     }
 
     _showTooltip() {
         this._tooltipContainer = document.createElement('div');
         this._tooltipContainer.textContent = this._tooltipText;
+        this._tooltipContainer.style.backgroundColor = 'black';
+        this._tooltipContainer.style.color = 'white';
+        this._tooltipContainer.style.position = 'absolute';
+        this._tooltipContainer.style.zIndex = '10';
+        //this.shadowRoot.appendChild(this._tooltipContainer);
         this.appendChild(this._tooltipContainer);
     }
 
     _hideTooltip() {
+        //this.shadowRoot.removeChild(this._tooltipContainer);
         this.removeChild(this._tooltipContainer);
     }
 
