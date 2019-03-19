3158127c39a7de4303b6830b9fbb883af0f9fbef
davi.leitao <dgleitao.contratos@gmail.com>
2019-03-19 00:22:08 -0300

    styling "slot" content inside shadow DOM
    For styling it is important to know that
    light DOM overrides the shadow DOM styling
    for projected or slotted content.
    
    Therefore, you could define some styling for
    projected or slotted content, but the final
    saying about which styling will prevail is
    from the light DOM.

--- tooltip.js
+++ tooltip.js
@@ -24,6 +24,13 @@ class Tooltip extends HTMLElement {
                     position: absolute;
                     z-index: 10;
                 }
+                .highlight {
+                    background-color: red;
+                }
+
+                ::slotted(.highlight) {
+                    border-bottom: 1px dotted red;
+                }
             </style>
             <slot>Some default value</slot>
             <span> (?)</span>
