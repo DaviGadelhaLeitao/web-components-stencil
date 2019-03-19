b7710f40268d827c4e7d788e9aa6cf70695b671d
davi.leitao <dgleitao.contratos@gmail.com>
2019-03-19 09:52:56 -0300

    Adding a conditional styling
    ... into the component that allows
    the client through the use of a css
    class apply some style rule.

--- tooltip.js
+++ tooltip.js
@@ -25,7 +25,7 @@ class Tooltip extends HTMLElement {
                     z-index: 10;
                 }
 
-                :host {
+                :host(.important) {
                     background: #ccc;
                 }
 
