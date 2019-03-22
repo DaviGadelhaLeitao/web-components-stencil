9419f5ec123c0f9aa648c631db48899354f049d0
Davi Gadelha Leitão <dgleitao.contratos@gmail.com>
2019-03-22 17:03:47 -0300

    closing the modal with modal buttons

--- modal.js
+++ modal.js
@@ -71,8 +71,8 @@ class Modal extends HTMLElement {
                   <slot></slot>
               </section>
               <section id="actions">
-                  <button>Cancel</button>
-                  <button>Okay</button>
+                  <button id="cancel-btn">Cancel</button>
+                  <button id="confirm-btn">Okay</button>
               </section>
           </div>
       `;
@@ -86,6 +86,11 @@ class Modal extends HTMLElement {
       // If you ever need to get access to the content that is projected into your slot.
       console.dir(slots[1].assignedNodes());
     });
+
+    const cancelButton = this.shadowRoot.querySelector("#cancel-btn");
+    const confirmButton = this.shadowRoot.querySelector("#confirm-btn");
+    cancelButton.addEventListener("click", this._cancel.bind(this));
+    confirmButton.addEventListener("click", this._confirm.bind(this));
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
@@ -107,6 +112,21 @@ class Modal extends HTMLElement {
   open() {
     this.setAttribute("opened", "");
     this.isOpen = true;
+  }
+
+  hide() {
+    if (this.hasAttribute("opened")) {
+      this.removeAttribute("opened");
+    }
+    this.isOpen = false;
+  }
+
+  _cancel() {
+    this.hide();
+  }
+
+  _confirm() {
+    this.hide();
   }
 }
 
