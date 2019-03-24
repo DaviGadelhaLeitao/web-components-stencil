7980199a8967985336a5a4dd75263bb57985184d
davi.leitao <dgleitao.contratos@gmail.com>
2019-03-23 23:47:48 -0300

    Adding an external file for styling
    Notice that we configured the scope of the css
    styling rules to the shadow DOM, the same thing
    we did when building a component natively.

--- stencil-project/src/components/side-drawer/side-drawer.tsx
+++ stencil-project/src/components/side-drawer/side-drawer.tsx
@@ -1,12 +1,16 @@
 import { Component } from '@stencil/core';
 
 @Component({
-    tag: 'uc-side-drawer'
+    tag: 'uc-side-drawer',
+    styleUrl: './side-drawer.css',
+    shadow: true
 })
 export class SideDrawer {
     render() {
         return (
-            <div><h1>The side drawer</h1></div>
+            <aside>
+                <h1>The side drawer</h1>
+            </aside>
         );
     }
 }
