acebaf16265575eb2c94249d6c4542557a99badf
davi.leitao <dgleitao.contratos@gmail.com>
2019-03-23 23:33:40 -0300

    Creating a new Stencil web component
    In this commit we use the bare minimal configuration
    enough to create and renders a web component to the screen.
    
    We just need to import the Component class, add a decorator
    to the Stencil component to inform that this a web component,
    put a tag on it, export this class which must contain at least
    a render() that returns the JSX.

---
+++ stencil-project/src/components/side-drawer/side-drawer.tsx
@@ -0,0 +1,13 @@
+import { Component } from '@stencil/core';
+
+@Component({
+    tag: 'uc-side-drawer'
+})
+export class SideDrawer {
+    render() {
+        return (
+            <div><h1>The side drawer</h1></div>
+        );
+    }
+}
+
