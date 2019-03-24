ffdec25ea9f1898e1f2f4241cfa06168e6f83886
davi.leitao <dgleitao.contratos@gmail.com>
2019-03-24 10:49:29 -0300

    Using @Prop() decorator
    This decorator annotation brings the functionally that if we set or change this
    attribute, or if we directly or programmatically set or change the title property
    from outside JavaScript then Stencil will automatically change this property and
    it will rerun the render() and will do so in a very efficiency manner so that is does
    not rerender the entire DOM that was generated based on it, but only the parts of the
    DOM that changed.
    
    This is what the @Prop() decorator does, it adds an automated watcher you could say.

--- stencil-project/src/components/side-drawer/side-drawer.tsx
+++ stencil-project/src/components/side-drawer/side-drawer.tsx
@@ -1,4 +1,4 @@
-import { Component } from '@stencil/core';
+import { Component, Prop } from '@stencil/core';
 
 @Component({
     tag: 'uc-side-drawer',
@@ -6,10 +6,22 @@ import { Component } from '@stencil/core';
     shadow: true
 })
 export class SideDrawer {
+    // this decoration annotation brings the functionally that if we set or change this
+    // attribute, or if we directly or programatically set or change the title property
+    // from outside JavaScript then Stencil will automatically this change and it will
+    // rerun the render() and will do so in a very efficiency manner so that is does not
+    // rerender the entire DOM that was generated based on it, but only the parts of the
+    // DOM that changed. This is what the @Prop() decorator does, it adds an automated
+    // watcher you could say. 
+    @Prop() title: string;
+
     render() {
         return (
             <aside>
-                <h1>The side drawer</h1>
+                <header><h1>{this.title}</h1></header>
+                <main>
+                    <slot></slot>
+                </main>
             </aside>
         );
     }
