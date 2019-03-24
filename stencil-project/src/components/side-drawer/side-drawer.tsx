fe5f28442cf5dacd276df23a4ace61f3a2453ad3
davi.leitao <dgleitao.contratos@gmail.com>
2019-03-24 10:56:41 -0300

    Configuring @Prop() with an object
    The reflectToAttr property makes the changed property have his HTML attribute value
    changed as well.

--- stencil-project/src/components/side-drawer/side-drawer.tsx
+++ stencil-project/src/components/side-drawer/side-drawer.tsx
@@ -6,14 +6,19 @@ import { Component, Prop } from '@stencil/core';
     shadow: true
 })
 export class SideDrawer {
-    // this decoration annotation brings the functionally that if we set or change this
-    // attribute, or if we directly or programatically set or change the title property
-    // from outside JavaScript then Stencil will automatically this change and it will
+
+    // This decoration annotation brings the functionally that if we set or change this
+    // attribute, or if we directly or programmatically set or change the title property
+    // from outside JavaScript then Stencil will automatically change this property and it will
     // rerun the render() and will do so in a very efficiency manner so that is does not
     // rerender the entire DOM that was generated based on it, but only the parts of the
-    // DOM that changed. This is what the @Prop() decorator does, it adds an automated
-    // watcher you could say. 
-    @Prop() title: string;
+    // DOM that changed.
+    
+    // This is what the @Prop() decorator does, it adds an automated watcher you could say.
+    
+    // The reflectToAttr property makes the changed property have his HTML attribute value
+    // changed as well.
+    @Prop({reflectToAttr: true}) title: string;
 
     render() {
         return (
