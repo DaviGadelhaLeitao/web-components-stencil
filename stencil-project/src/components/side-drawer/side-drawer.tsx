74d187746e36a035723cc72c81030e000855cfed
davi.leitao <dgleitao.contratos@gmail.com>
2019-03-24 22:45:23 -0300

    Adding more event listeners
    ...to watch for when the user clicks on navigation
    or contact button.

--- stencil-project/src/components/side-drawer/side-drawer.tsx
+++ stencil-project/src/components/side-drawer/side-drawer.tsx
@@ -19,13 +19,28 @@ export class SideDrawer {
     // The reflectToAttr property makes the changed property have his HTML attribute value
     // changed as well.
     @Prop({reflectToAttr: true}) title: string;
-    @Prop({reflectToAttr: true}) open: boolean;
+    @Prop({reflectToAttr: true, mutable: true}) open: boolean;
 
     onCloseDrawer() {
         this.open = false;
     }
 
+    onContentChange(content: string) {
+        console.log(content);
+    }
+
     render() {
+        let mainContent = <slot />;
+        mainContent = (
+            <div id="contact-information">
+                <h2>Contact Information</h2>
+                <p>You can reach us via phone or email.</p>
+                <ul>
+                    <li>Phone: 908432984</li>
+                    <li>Email <a href="mailto:something@something.com">something@something.com</a></li>
+                </ul>
+            </div>
+        );
         return (
             <aside>
                 <header>
@@ -33,11 +48,11 @@ export class SideDrawer {
                     <button onClick={this.onCloseDrawer.bind(this)}>X</button>
                 </header>
                 <section id="tabs">
-                    <button class="active">Navigation</button>
-                    <button>Contact</button>
+                    <button class="active" onClick={this.onContentChange.bind(this, 'nav')}>Navigation</button>
+                    <button onClick={this.onContentChange.bind(this, 'contact')}>Contact</button>
                 </section>
                 <main>
-                    <slot></slot>
+                    {mainContent}
                 </main>
             </aside>
         );
