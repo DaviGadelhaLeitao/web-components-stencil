e79a837808b8647e44775d98678a298b3e2485c4
davi.leitao <dgleitao.contratos@gmail.com>
2019-03-24 23:24:24 -0300

    Using state to manage changes into inside props
    If only expect to receive changes from inside you should definitely use a different
    decorator, the @State().

--- stencil-project/src/components/side-drawer/side-drawer.tsx
+++ stencil-project/src/components/side-drawer/side-drawer.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop } from '@stencil/core';
+import { Component, Prop, State } from '@stencil/core';
 
 @Component({
     tag: 'uc-side-drawer',
@@ -7,17 +7,7 @@ import { Component, Prop } from '@stencil/core';
 })
 export class SideDrawer {
 
-    // This decoration annotation brings the functionally that if we set or change this
-    // attribute, or if we directly or programmatically set or change the title property
-    // from outside JavaScript then Stencil will automatically change this property and it will
-    // rerun the render() and will do so in a very efficiency manner so that is does not
-    // rerender the entire DOM that was generated based on it, but only the parts of the
-    // DOM that changed.
-    
-    // This is what the @Prop() decorator does, it adds an automated watcher you could say.
-    
-    // The reflectToAttr property makes the changed property have his HTML attribute value
-    // changed as well.
+    @State() showContactInfo = false;
     @Prop({reflectToAttr: true}) title: string;
     @Prop({reflectToAttr: true, mutable: true}) open: boolean;
 
@@ -26,21 +16,23 @@ export class SideDrawer {
     }
 
     onContentChange(content: string) {
-        console.log(content);
+        this.showContactInfo = content === 'contact';
     }
 
     render() {
         let mainContent = <slot />;
-        mainContent = (
-            <div id="contact-information">
-                <h2>Contact Information</h2>
-                <p>You can reach us via phone or email.</p>
-                <ul>
-                    <li>Phone: 908432984</li>
-                    <li>Email <a href="mailto:something@something.com">something@something.com</a></li>
-                </ul>
-            </div>
-        );
+        if (this.showContactInfo) {
+            mainContent = (
+                <div id="contact-information">
+                    <h2>Contact Information</h2>
+                    <p>You can reach us via phone or email.</p>
+                    <ul>
+                        <li>Phone: 908432984</li>
+                        <li>Email: <a href="mailto:something@something.com">something@something.com</a></li>
+                    </ul>
+                </div>
+            );
+        }
         return (
             <aside>
                 <header>
@@ -48,8 +40,8 @@ export class SideDrawer {
                     <button onClick={this.onCloseDrawer.bind(this)}>X</button>
                 </header>
                 <section id="tabs">
-                    <button class="active" onClick={this.onContentChange.bind(this, 'nav')}>Navigation</button>
-                    <button onClick={this.onContentChange.bind(this, 'contact')}>Contact</button>
+                    <button class={!this.showContactInfo ? 'active' : '' } onClick={this.onContentChange.bind(this, 'nav')}>Navigation</button>
+                    <button class={this.showContactInfo ? 'active' : '' } onClick={this.onContentChange.bind(this, 'contact')}>Contact</button>
                 </section>
                 <main>
                     {mainContent}
