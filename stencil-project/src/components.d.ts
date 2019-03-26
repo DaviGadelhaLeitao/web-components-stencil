c4a74695b0336aadbbdfa8e758442ede240dfae4
davi.leitao <dgleitao.contratos@gmail.com>
2019-03-25 23:34:25 -0300

    creating a new component and submitting a form...

--- stencil-project/src/components.d.ts
+++ stencil-project/src/components.d.ts
@@ -22,6 +22,9 @@ export namespace Components {
     'title'?: string;
   }
 
+  interface UcStockPrice {}
+  interface UcStockPriceAttributes extends StencilHTMLAttributes {}
+
   interface UcTooltipStencil {
     'text': string;
   }
@@ -33,11 +36,13 @@ export namespace Components {
 declare global {
   interface StencilElementInterfaces {
     'UcSideDrawer': Components.UcSideDrawer;
+    'UcStockPrice': Components.UcStockPrice;
     'UcTooltipStencil': Components.UcTooltipStencil;
   }
 
   interface StencilIntrinsicElements {
     'uc-side-drawer': Components.UcSideDrawerAttributes;
+    'uc-stock-price': Components.UcStockPriceAttributes;
     'uc-tooltip-stencil': Components.UcTooltipStencilAttributes;
   }
 
@@ -48,6 +53,12 @@ declare global {
     new (): HTMLUcSideDrawerElement;
   };
 
+  interface HTMLUcStockPriceElement extends Components.UcStockPrice, HTMLStencilElement {}
+  var HTMLUcStockPriceElement: {
+    prototype: HTMLUcStockPriceElement;
+    new (): HTMLUcStockPriceElement;
+  };
+
   interface HTMLUcTooltipStencilElement extends Components.UcTooltipStencil, HTMLStencilElement {}
   var HTMLUcTooltipStencilElement: {
     prototype: HTMLUcTooltipStencilElement;
@@ -56,11 +67,13 @@ declare global {
 
   interface HTMLElementTagNameMap {
     'uc-side-drawer': HTMLUcSideDrawerElement
+    'uc-stock-price': HTMLUcStockPriceElement
     'uc-tooltip-stencil': HTMLUcTooltipStencilElement
   }
 
   interface ElementTagNameMap {
     'uc-side-drawer': HTMLUcSideDrawerElement;
+    'uc-stock-price': HTMLUcStockPriceElement;
     'uc-tooltip-stencil': HTMLUcTooltipStencilElement;
   }
 
---
+++ stencil-project/src/components/stock-price/stock-price.tsx
@@ -0,0 +1,27 @@
+import { Component } from '@stencil/core';
+@Component({
+    tag: 'uc-stock-price',
+    styleUrl: './stock-price.css',
+    shadow: true
+})
+export class StockPrice {
+
+    onFetchStockPrice(event: Event) {
+        event.preventDefault();
+        console.log("submitted");
+    }
+
+    render() {
+        return [
+            <form onSubmit={this.onFetchStockPrice}>
+                <input type="text" id="stock-symbol"/>
+                <button type="submit">Fetch</button>
+            </form>,
+            <div>
+                <p>Price: {0}</p>
+            </div>
+
+        ];
+    }
+
+}
