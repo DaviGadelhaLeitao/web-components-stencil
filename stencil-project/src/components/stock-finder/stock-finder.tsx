d9abc6c3ab372389225c7b0d197faa8cbf8d6809
davi.leitao <dgleitao.contratos@gmail.com>
2019-03-31 10:24:00 -0300

    Emitting custom events
    In this commit, we created a new class property of type EventEmitter.
    
    This property it's used to emit an event when a user clicks on one of the
    list items, this event is then listened to by the web application for
    now.
    
    Next commit we'll make this event be listened directly from another
    component if present.

--- stencil-project/src/components/stock-finder/stock-finder.tsx
+++ stencil-project/src/components/stock-finder/stock-finder.tsx
@@ -1,4 +1,4 @@
-import { Component, State } from '@stencil/core';
+import { Component, State, Event, EventEmitter } from '@stencil/core';
 import { AV_API_KEY } from '../../global/global';
 @Component({
     tag: 'uc-stock-finder',
@@ -9,6 +9,8 @@ export class StockFinder {
     stockNameInput: HTMLInputElement;
 
     @State() searchResults: { symbol: string, name: string }[] = [];
+
+    @Event({bubbles: true, composed: true}) ucSymbolSelected: EventEmitter<string>; 
 
     onFindStocks(event: Event) {
         event.preventDefault();
@@ -22,7 +24,11 @@ export class StockFinder {
         })
         .catch(err => {
             console.log(err);
-        })
+        });
+    }
+
+    onSelectSymbol(symbol: string) {
+        this.ucSymbolSelected.emit(symbol);
     }
 
     render() {
@@ -39,7 +45,7 @@ export class StockFinder {
             </form>,
             <ul>
                 {this.searchResults.map(result => (
-                    <li><strong>{result.symbol}</strong> - {result.name}</li>
+                    <li onClick={this.onSelectSymbol.bind(this, result.symbol)}><strong>{result.symbol}</strong> - {result.name}</li>
                 ))}
             </ul>
         ];
